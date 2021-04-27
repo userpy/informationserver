f4205c1d48426ea4657d902487b970da6c40d559
userpy <dima6225@yandex.ru>
2021-04-28 02:14:08 +0300

    add reflect

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Домашняя страница
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
+  name: 'Index',
   components: {
-    HelloWorld
+
   }
 }
 </script>
