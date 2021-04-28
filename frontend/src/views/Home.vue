861b173d47c3de6d656783def222982e534186be
userpy <dima6225@yandex.ru>
2021-04-28 13:17:20 +0300

    add vuetify and axios

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,12 +4,19 @@
 
 <script>
   import HelloWorld from '../components/HelloWorld'
-
+  import axios from 'axios'
   export default {
     name: 'Home',
 
     components: {
       HelloWorld,
     },
+    created() {
+      axios.get('https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8').catch(val => {
+         console.log(`Ошибка =====${val}\n`)
+      }).then(val => {
+        console.log(` test axios==========${JSON.stringify(val)}\n`)
+      })
+    }
   }
 </script>
