324e1636731b3d55aea799dacadd23cf3f429256
userpy <dima6225@yandex.ru>
2021-04-28 11:41:26 +0300

    add vuetify and axios

--- app/auth.go
+++ app/auth.go
@@ -3,64 +3,15 @@ package app
 import (
 	"context"
 	"fmt"
-	set "github.com/deckarep/golang-set"
 	jwt "github.com/dgrijalva/jwt-go"
 	"informationserver/models"
 	u "informationserver/utils"
 	"net/http"
 	"os"
-	"reflect"
-	"regexp"
 	"strings"
 )
 
-//Вхождение маршрута  проверку регистрации, добавление роли
-func CheckUrlPath(val string, array map[string]interface{}, role *[]interface{}) (exists bool) {
-	exists = false
-	for pattern, role_from_pattern := range array {
-		matched, _ := regexp.Match(pattern, []byte(val))
-		if matched {
-			v := reflect.ValueOf(role_from_pattern)
-			switch v.Kind() {
-			case reflect.Slice:
-				for i := 0; i < v.Len(); i++ {
-					fmt.Printf("+++ %s %s\n", i, v.Index(i).String())
-					*role = append(*role, v.Index(i).String())
-				}
-			case reflect.Bool:
-				*role = append(*role, role_from_pattern)
-			}
-			exists = true
-			return exists
-		}
-	}
-	return exists
-}
-
-//Перевод массива интерфейсов в массив строк
-func ArrInterfaceToArrayString(interface_arr []interface{}) (string_arr []interface{}) {
-	for _, v := range interface_arr {
-		string_arr = append(string_arr, fmt.Sprintf("%s", v))
-	}
-	return
-}
-
-// Проверка роли пользователя
-func CheckRole(role_token []interface{}, role_check_in_server []interface{}) (exist bool) {
-	if role_check_in_server[0] == false {
-		exist = false
-		return
-	} else {
-		if len(set.NewSetFromSlice(ArrInterfaceToArrayString(role_token)).Intersect(set.NewSetFromSlice(ArrInterfaceToArrayString(role_check_in_server))).ToSlice()) != 0 {
-			fmt.Printf("Пересекаются\n")
-			exist = false
-			return
-		}
-		exist = true
-		return
-	}
-}
-
+//Проверка JWT -токена
 var JwtAuthentication = func(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		//Список эндпоинтов, для которых  требуется авторизация, с проверкой роли или без
@@ -94,7 +45,6 @@ var JwtAuthentication = func(next http.Handler) http.Handler {
 
 			tokenPart := splitted[1] //Получаем вторую часть токена
 			tk := &models.Token{}
-			//fmt.Printf("- %s\n", tokenPart)
 			token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (interface{}, error) {
 				return []byte(os.Getenv("token_password")), nil
 			})
@@ -125,7 +75,6 @@ var JwtAuthentication = func(next http.Handler) http.Handler {
 			}
 			ctx := context.WithValue(r.Context(), "user", tk.UserId)
 			r = r.WithContext(ctx)
-			//fmt.Printf(">>>>>>\n")
 			next.ServeHTTP(w, r) //передать управление следующему обработчику!
 
 		} else {
