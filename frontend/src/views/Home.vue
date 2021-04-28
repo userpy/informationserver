ed04b55bcc077cc1da62f69f08a993bf1265258d
userpy <dima6225@yandex.ru>
2021-04-28 13:25:53 +0300

    add vuetify and axios

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -15,7 +15,7 @@
       axios.get('https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8').catch(val => {
          console.log(`Ошибка =====${val}\n`)
       }).then(val => {
-        console.log(` test axios==========${JSON.stringify(val)}\n`)
+        console.log(` test axios ==========${JSON.stringify(val)}\n`)
       })
     }
   }
